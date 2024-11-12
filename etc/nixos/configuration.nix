6de296c59ce8fc0dee2bce6335bf9050f10d56d7
komputerman <tam.sebastian@me.com>
2024-11-12 10:23:59 +0100

    Changed some Settings which not suite to all systems

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -106,7 +106,7 @@
   environment.systemPackages = with pkgs; [
   	htop
 	neovim
-        spotify
+    spotify
 	sway
 	swaylock-fancy
 	parted
@@ -142,22 +142,22 @@ services.mullvad-vpn.enable = true;
   
   # Andere Konfigurationen...
 
-  fileSystems."/mnt/HDD_2TB" = {
-    device = "/dev/disk/by-uuid/0175bd66-c6a2-4376-a86b-996d1a795e20";
-    fsType = "ext4";  # Oder das entsprechende Dateisystem Ihrer Platte
-    options = [ "defaults" "nofail" "x-systemd.automount" ];
-  };
+  #fileSystems."/mnt/HDD_2TB" = {
+  #  device = "/dev/disk/by-uuid/0175bd66-c6a2-4376-a86b-996d1a795e20";
+  #  fsType = "ext4";  # Oder das entsprechende Dateisystem Ihrer Platte
+  #  options = [ "defaults" "nofail" "x-systemd.automount" ];
+  #};
 
   # Berechtigungen für alle Benutzer setzen
-  system.activationScripts = {
-    setPermissions = {
-      text = ''
-        mkdir -p /mnt/HDD_2TB
-        chmod 1777 /mnt/HDD_2TB
-      '';
-      deps = [];
-    };
-  };
+  #system.activationScripts = {
+  #  setPermissions = {
+  #    text = ''
+  #      mkdir -p /mnt/HDD_2TB
+  #      chmod 1777 /mnt/HDD_2TB
+  #    '';
+  #    deps = [];
+  #  };
+  #};
   
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
